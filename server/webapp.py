d63686fd6bb2e9339d62130eb0158db71dfc6525
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-01-20 20:49:11 +0900

    Import layout.html

--- server/webapp.py
+++ server/webapp.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def root():
-    return "This is root!"
+def home():
+    return render_template("layout.html")
+
+
+@app.route('/runs')
+def runs():
+    return "Run Lists"
+
+
+@app.route("/projects")
+def projects():
+    return "Project Lists"
+
+
+@app.route("/ipynbs")
+def ipynbs():
+    return "IPython Notebooks"
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
