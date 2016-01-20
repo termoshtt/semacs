0d03f1921e65e25ea435116c5141a49e10290d16
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-01-20 18:39:55 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name="semacs",
+    version="0.1.0",
+    description="semantic data manager",
+    author="Toshiki Teramura",
+    author_email="toshiki.teramura+continuate@gmail.com",
+    license="MIT",
+    keywords="science",
+    url="http://github.com/termoshtt/semacs",
+    packages=["semacs"],
+)
